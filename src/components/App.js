4cb0ee1dff4abf3822f84c22ee609086deb8acd2
Irving Ventura <irvingv8@gmail.com>
2017-11-25 20:47:07 -0400

    -> set up styled components / removed CRA placeholders

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { injectGlobal, ThemeProvider } from "styled-components";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+const theme = {};
+
+injectGlobal`
+  body {
+    margin: 0;
   }
-}
+`;
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        GET SHIT DONE
+        <br />
+        &
+        <br />
+        ROCK THOSE FUCKING DISHES!
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default App;
